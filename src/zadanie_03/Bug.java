3880112d5c5f36bd250e7a680e62e571c563b6f5
michal-d-o <michal.dudczenko@gmail.com>
2020-12-03 00:38:46 +0100

    zadanie 03

---
+++ src/zadanie_03/Bug.java
@@ -0,0 +1,76 @@
+package zadanie_03;
+
+public class Bug {
+
+    private String bugDescription;
+    private String email;
+    private int bugPriority;
+    private boolean statusOpen;
+
+    public Bug(String description, String email, int priority) {
+        this.bugDescription = description;
+        this.email = email;
+        this.bugPriority = priority;
+        this.statusOpen = true;
+    }
+
+    public String getBugDescription() {
+        return bugDescription;
+    }
+
+    public void setBugDescription(String bugDescription) {
+        if (bugDescription.length() > 9) {
+            this.bugDescription = bugDescription;
+        } else {
+            System.out.println("Error - bug description must contain at least 10 characters.");
+        }
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if (email.contains("@")) {
+            this.email = email;
+        } else {
+            System.out.println("Error - email address should contain the @ sign.");
+        }
+
+    }
+
+    public int getBugPriority() {
+        return bugPriority;
+    }
+
+    public void setBugPriority(int bugPriority) {
+        if (bugPriority < 6 && bugPriority > 0) {
+            this.bugPriority = bugPriority;
+        } else {
+            System.out.println("Error - bug priority has to be a single digit between 1 and 5.");
+        }
+    }
+
+    public boolean isStatusOpen() {
+        return statusOpen;
+    }
+
+    public void setStatusOpen(boolean statusOpen) {
+        this.statusOpen = statusOpen;
+    }
+
+
+    //
+    public void showBugInfo() {
+        System.out.println("zadanie_03.Bug: " + bugDescription + ", " + email + ", " + bugPriority + ", zadanie_03.Bug status 'open'- " + statusOpen);
+    }
+
+    public void showEmail() {
+        System.out.println("Reporter's email: " + email);
+    }
+
+    public void showStatus() {
+        System.out.println("Is status 'open'? - " + statusOpen);
+    }
+
+}
